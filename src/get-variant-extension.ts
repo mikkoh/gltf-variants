7de91435e83534623f2cfaa4d0c54fce34532417
mikkoh <me@mikkoh.com>
2019-04-18 11:24:35 -0400

    Removed references to node from get-variant-extension

--- src/get-variant-extension.ts
+++ src/get-variant-extension.ts
@@ -1,14 +1,14 @@
 import { IChildRootProperty } from "babylonjs-gltf2interface";
 import { ITaggedChildRootProperty } from './inode-with-variant-extension';
 
-export default function getVariantExtension(node: IChildRootProperty): ITaggedChildRootProperty[] {
-  if (!node.extensions) {
-    node.extensions = {};
+export default function getVariantExtension(item: IChildRootProperty): ITaggedChildRootProperty[] {
+  if (!item.extensions) {
+    item.extensions = {};
   }
 
-  if (!node.extensions.SHOPIFY_variant) {
-    node.extensions.SHOPIFY_variant = [];
+  if (!item.extensions.SHOPIFY_variant) {
+    item.extensions.SHOPIFY_variant = [];
   }
 
-  return node.extensions.SHOPIFY_variant;
+  return item.extensions.SHOPIFY_variant;
 }
